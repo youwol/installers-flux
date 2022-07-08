be3eda511961175a16f83204c197495cd9e4cd10
greinisch <greinisch@youwol.com>
2022-07-08 13:11:21 +0200

    ✨ add publish-app modal

--- src/lib/basics/installer.ts
+++ src/lib/basics/installer.ts
@@ -1,10 +1,12 @@
 import {
     AssetsGateway,
     ExplorerBackend,
+    FluxBackend,
     raiseHTTPErrors,
 } from '@youwol/http-clients'
 import { FluxDependenciesView } from './flux-dependencies.view'
-import { AssetLightDescription } from '@youwol/os-core/dist'
+import { AssetLightDescription } from '@youwol/os-core'
+import { mergeMap } from 'rxjs/operators'
 
 export function contextMenuActions({ node, explorer, assetsGtwClient }) {
     return [
@@ -21,6 +23,16 @@ export function contextMenuActions({ node, explorer, assetsGtwClient }) {
             enabled: () => true,
             exe: async () =>
                 duplicateFluxProject(node, explorer, assetsGtwClient),
+            applicable: () =>
+                ExplorerBackend.isInstanceOfItemResponse(node) &&
+                node.kind == 'flux-project',
+        },
+        {
+            name: 'Publish as application',
+            icon: { class: 'fas fa-clone' },
+            enabled: () => true,
+            exe: async () =>
+                publishFluxProject(node, explorer, assetsGtwClient),
             applicable: () =>
                 ExplorerBackend.isInstanceOfItemResponse(node) &&
                 node.kind == 'flux-project',
@@ -81,4 +93,43 @@ async function duplicateFluxProject(
                 }),
             })
         })
+}
+
+async function publishFluxProject(
+    projectNode,
+    _explorer,
+    assetsGtwClient: AssetsGateway.Client,
+) {
+    // The modal will be displayed in the root document => we use '@youwol/cdn-client' of the root window
+    const { codeEditorsModule } = await window['@youwol/cdn-client'].install({
+        modules: ['@youwol/fv-code-mirror-editors', '@youwol/fv-group'],
+        scripts: [
+            'codemirror#5.52.0~mode/javascript.min.js',
+            'codemirror#5.52.0~addons/lint/lint.js',
+        ],
+        css: [
+            'codemirror#5.52.0~codemirror.min.css',
+            'codemirror#5.52.0~theme/blackboard.min.css',
+            'codemirror#5.52.0~addons/lint/lint.css',
+        ],
+        aliases: {
+            codeEditorsModule: '@youwol/fv-code-mirror-editors',
+            fvGroup: '@youwol/fv-group',
+        },
+    })
+    const publishAppMdl = await import('./publish-app.view')
+    publishAppMdl
+        .popupModal(codeEditorsModule)
+        .pipe(
+            mergeMap((body: FluxBackend.PublishApplicationBody) => {
+                return assetsGtwClient.flux.publishApplication$({
+                    projectId: projectNode.rawId as string,
+                    body,
+                    queryParameters: {
+                        folderId: projectNode.folderId as string,
+                    },
+                })
+            }),
+        )
+        .subscribe()
 }
---
+++ src/lib/basics/publish-app.view.ts
@@ -0,0 +1,203 @@
+import { VirtualDOM, HTMLElement$, render } from '@youwol/flux-view'
+import { BehaviorSubject, merge, Observable } from 'rxjs'
+import { Modal } from '@youwol/fv-group'
+import { map, mergeMap, take } from 'rxjs/operators'
+import { CodeIdeState } from '@youwol/fv-code-mirror-editors'
+import { FluxBackend, request$, onHTTPErrors } from '@youwol/http-clients'
+import { TsCodeEditorView } from '@youwol/fv-code-mirror-editors/src/lib/ts-code-editor.view'
+
+const defaultMetadata = {
+    name: '>TBD<',
+    displayName: '>TBD<',
+    version: '>TBD<',
+    execution: {
+        standalone: true,
+        parametrized: [
+            /*{
+            "match":{
+               "kind":"story"
+            } // can be e.g.: { "match": (asset) => asset.name.endsWith(".my-extension") },
+            "parameters":{
+               "id":"rawId"
+            }
+         }*/
+        ],
+    },
+    graphics: {
+        appIcon: {
+            class: 'fas fa-book fa-2x',
+        },
+        fileIcon: {
+            class: 'fas fa-book',
+        },
+        background: {},
+    },
+}
+
+export class PublishAppState implements VirtualDOM {
+    public readonly name$: BehaviorSubject<string>
+    public readonly title$: BehaviorSubject<string>
+    public readonly version$: BehaviorSubject<string>
+
+    constructor({
+        name,
+        title,
+        version,
+    }: {
+        name: string
+        title: string
+        version: string
+    }) {
+        this.name$ = new BehaviorSubject(name)
+        this.title$ = new BehaviorSubject(title)
+        this.version$ = new BehaviorSubject(version)
+    }
+}
+
+function createEditor(tsSrcs: string, codeEditorsModule) {
+    const ideState: CodeIdeState = new codeEditorsModule.CodeIdeState({
+        files: {
+            path: './index.ts',
+            content: tsSrcs,
+        },
+        entryPoint: './index.ts',
+    })
+    const view: TsCodeEditorView = new codeEditorsModule.TsCodeEditorView({
+        ideState,
+    })
+    view.nativeEditor$.subscribe((editor) => {
+        editor.refresh()
+    })
+    return view
+}
+
+function metadataToSrc(metadata: { [k: string]: any }) {
+    return `return () => (\n\t ${JSON.stringify(metadata, null, 4).replace(
+        /">TBD<"/g,
+        '',
+    )} )\n`
+}
+
+export class FindTemplateView implements VirtualDOM {
+    public readonly class =
+        'd-flex justify-content-center mb-2 align-items-center'
+    public readonly children: VirtualDOM[]
+    public readonly codeIdeState: CodeIdeState
+    constructor(params: { codeIdeState: CodeIdeState }) {
+        Object.assign(this, params)
+        this.children = [
+            {
+                innerText: 'find template',
+                class: 'fv-text-primary mx-2',
+            },
+            {
+                tag: 'input',
+                type: 'text',
+                onkeypress: (ev: KeyboardEvent) => {
+                    const target = ev.target as HTMLInputElement
+                    const resourceId = window.btoa(target.value)
+                    if (ev.key == 'Enter') {
+                        request$(
+                            new Request(
+                                `/api/assets-gateway/cdn-backend/resources/${resourceId}/latest/.yw_metadata.json`,
+                            ),
+                        )
+                            .pipe(
+                                onHTTPErrors(() => defaultMetadata),
+                                map((respJson: { [k: string]: any }) => {
+                                    delete respJson['family']
+                                    return {
+                                        name: target.value,
+                                        version: '>TBD<',
+                                        ...respJson,
+                                    }
+                                }),
+                            )
+                            .subscribe((resp: { [k: string]: any }) => {
+                                const content = metadataToSrc(resp)
+                                this.codeIdeState.currentFile$.next({
+                                    path: `./index${Math.random() * 1e6}.ts`,
+                                    content,
+                                })
+                            })
+                    }
+                },
+            },
+            {
+                tag: 'i',
+                class: 'fas fa-search mx-2 fv-text-primary',
+            },
+        ]
+    }
+}
+export function popupModal(
+    codeEditorsModule,
+): Observable<FluxBackend.PublishApplicationBody> {
+    const modalState = new Modal.State()
+    const validateBtn = {
+        class: 'w-100 d-flex justify-content-center',
+        children: [
+            {
+                class: 'border fv-bg-secondary rounded p-1 fv-pointer fv-hover-xx-lighter fv-text-primary fv-btn mt-2',
+                children: [
+                    {
+                        class: 'fas fa-box',
+                    },
+                ],
+                onclick: (ev) => {
+                    modalState.ok$.next(ev)
+                },
+            },
+        ],
+    }
+    const editor = createEditor(
+        metadataToSrc(defaultMetadata),
+        codeEditorsModule,
+    )
+
+    const modalView = new Modal.View({
+        state: modalState,
+        contentView: () => {
+            return {
+                class: 'd-flex flex-column',
+                style: {
+                    width: '75vw',
+                    height: '75vh',
+                },
+                children: [
+                    {
+                        class: 'flex-grow-1',
+                        children: [
+                            new FindTemplateView({
+                                codeIdeState: editor.ideState,
+                            }),
+                        ],
+                    },
+                    editor,
+                    validateBtn,
+                ],
+                onclick: (ev: MouseEvent) => ev.stopPropagation(),
+            }
+        },
+        connectedCallback: (elem: HTMLDivElement & HTMLElement$) => {
+            elem.children[0].classList.add('w-100')
+            // https://stackoverflow.com/questions/63719149/merge-deprecation-warning-confusion
+            merge(...[modalState.cancel$, modalState.ok$])
+                .pipe(take(1))
+                .subscribe(() => {
+                    modalDiv.remove()
+                })
+        },
+    })
+    const modalDiv = render(modalView)
+    document.querySelector('body').appendChild(modalDiv)
+
+    return modalState.ok$.pipe(
+        mergeMap(() => {
+            return editor.ideState.parseCurrentFile$()
+        }),
+        map(({ jsSrc }) => {
+            return new Function(jsSrc)()()
+        }),
+    )
+}
